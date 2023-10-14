a74916465633ed3fd053828134b2725cbc9559c4
PLUTo <nawin3399@gmail.com>
2023-10-15 02:31:02 +0700

    add reportOrder function

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,12 +11,14 @@ use Illuminate\Support\Facades\Session;
 class OrderController extends Controller
 {
 
-    public function index () {
+    public function index()
+    {
         $orders = Order::all();
         return view('order/index', compact('orders'));
     }
 
-    public function insertOrder(){
+    public function insertOrder()
+    {
         $orders = new Order();
         $orders->ref_id = Session::get('po_no');
         $orders->user_id = Auth::user()->id;
@@ -27,14 +29,14 @@ class OrderController extends Controller
         // return redirect('cart/finish');
 
     }
-  
 
-    public function insertDetail() {
+    public function insertDetail()
+    {
         $cart_items = Session::get('cart_items');
         // $orders = Order::where('ref_id', 'like' , Session::get('po_no'))->get();
-        $orders = Order::where('ref_id', 'like' ,Session::get('po_no'))->get();
+        $orders = Order::where('ref_id', 'like', Session::get('po_no'))->get();
         $orders_id = $orders[0]->id;
-        foreach($cart_items as $c){
+        foreach ($cart_items as $c) {
             $orders_detail = new Order_Detail();
             $orders_detail->product_id = $c['id'];
             $orders_detail->order_id = $orders_id;
@@ -45,5 +47,49 @@ class OrderController extends Controller
         return redirect('cart/finish');
     }
 
+    public function reportOrder($ref_id)
+    {
+        $order = Order::where('ref_id', 'like', '%' .$ref_id. '%')->first();
+        $order->user;
+        $cust_name = $order->user->name;
+        $cust_email = $order->user->email;
+        $po_no = $ref_id;
+        $po_date = $order->created_at->format("Y-m-d H:i:s");
+        $total_amount = 0;
+        $cart_items = array();
+
+        $details = Order_detail::where('order_id', 'like', '%' . $order->id . '%')->get();
+        foreach($details as $d) {
+            $cart_items[$d->product->id] = array(
+                "id" => $d->product->id,
+                "code" => $d->product->code,
+                "name" => $d->product->name,
+                "price" => $d->product->price,
+                "image_url" => $d->product->image_url,
+                "qty" => $d->qty
+
+            ); 
+            $total_amount += $d->product->price * $d->qty;
+        }
     
+
+        $html_output = view(
+            'cart/complete',
+            compact(
+                'cart_items',
+                'cust_name',
+                'cust_email',
+                'po_no',
+                'po_date',
+                'total_amount'
+            )
+        )->render();
+        $mpdf = new \Mpdf\Mpdf();
+        $mpdf->debug = true;
+        $mpdf->WriteHTML($html_output);
+        $mpdf->Output('output.pdf', 'I');
+        return $resp->withHeader("Content-type", "application/pdf");
+    }
+
+
 }
