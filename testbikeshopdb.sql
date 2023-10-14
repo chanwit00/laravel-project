020fdf14350a0dc900742031e621ed5f40925213
kenjung <j.pannawat45@gmail.com>
2023-10-14 22:40:12 +0700

    database

--- testbikeshopdb.sql
+++ testbikeshopdb.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 27, 2023 at 03:28 PM
--- Server version: 10.11.0-MariaDB
+-- Generation Time: Oct 14, 2023 at 03:39 PM
+-- Server version: 8.0.17
 -- PHP Version: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bikeshopdb`
+-- Database: `testbikeshopdb`
 --
 
 -- --------------------------------------------------------
@@ -40,7 +40,7 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'อะไหล่', NULL, '2023-08-27 04:59:50'),
+(1, 'อะไหล่', NULL, NULL),
 (2, 'เครื่องแต่งกาย', NULL, NULL),
 (3, 'รองเท้า', NULL, NULL),
 (4, 'แว่นตา', NULL, NULL),
@@ -60,7 +60,7 @@ CREATE TABLE `failed_jobs` (
   `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -80,13 +80,58 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(14, '2023_08_23_161649_create_test_table', 1),
-(39, '2014_10_12_000000_create_users_table', 2),
-(40, '2014_10_12_100000_create_password_resets_table', 2),
-(41, '2019_08_19_000000_create_failed_jobs_table', 2),
-(42, '2019_12_14_000001_create_personal_access_tokens_table', 2),
-(43, '2023_07_20_021157_create_category_table', 2),
-(44, '2023_07_20_021417_create_product_table', 2);
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2023_07_20_021157_create_category_table', 1),
+(6, '2023_07_20_021417_create_product_table', 1),
+(7, '2023_10_14_110617_create_order_table', 1),
+(8, '2023_10_14_110629_create_order_detail_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `ref_id` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `status` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`id`, `ref_id`, `user_id`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'PO202310141', 1, 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_detail`
+--
+
+CREATE TABLE `order_detail` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `product_id` int(10) UNSIGNED NOT NULL,
+  `order_id` int(10) UNSIGNED NOT NULL,
+  `qty` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `order_detail`
+--
+
+INSERT INTO `order_detail` (`id`, `product_id`, `order_id`, `qty`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 10, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -112,7 +157,7 @@ CREATE TABLE `personal_access_tokens` (
   `tokenable_id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
   `last_used_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -141,29 +186,9 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `code`, `name`, `category_id`, `price`, `stock_qty`, `image_url`, `created_at`, `updated_at`) VALUES
-(1, '007', 'เสื้อยืด Uniqlo Size XS', 2, 490.00, 10, 'upload/image/1', NULL, '2023-08-27 04:51:59'),
-(2, '001', 'เสื้อยืด Uniqlo Size S', 2, 490.00, 10, 'upload/image/2', NULL, '2023-08-27 04:53:11'),
-(3, '002', 'เสื้อยืด Uniqlo Size M', 2, 490.00, 10, 'upload/image/3', NULL, '2023-08-27 04:53:16'),
-(4, '003', 'เสื้อยืด Uniqlo Size L', 2, 520.00, 10, 'upload/image/4', NULL, '2023-08-27 04:53:22'),
-(5, '008', 'เสื้อยืด Uniqlo Size XL', 2, 520.00, 10, 'upload/image/5', NULL, '2023-08-27 04:53:27'),
-(6, '009', 'เสื้อยืด Uniqlo Size 2XL', 2, 550.00, 10, NULL, NULL, NULL),
-(7, '010', 'เสื้อยืด Uniqlo Size 3XL', 2, 550.00, 10, NULL, NULL, NULL),
-(8, '004', 'ล้อรถยนต์', 1, 2500.00, 10, NULL, NULL, NULL),
-(9, '005', 'พวงมาลัย', 1, 4900.00, 10, NULL, NULL, NULL),
-(10, '006', 'Nike air force 1 เบอร์ 38 US', 3, 1490.00, 10, NULL, NULL, NULL),
-(11, 'XXX', 'XXX', 5, 500.00, 50, 'upload/image/11', '2023-08-27 04:55:40', '2023-08-27 04:55:40');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `test`
---
-
-CREATE TABLE `test` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+(1, 'P001', 'เสื้อยืด', 1, 11900.00, 2, 'upload/image/1', NULL, '2023-10-14 08:20:26'),
+(2, 'P002', 'เสื้อหมอบ', 1, 5000.00, 2, 'upload/image/2', NULL, '2023-10-14 08:20:34'),
+(3, 'P003', 'เสื้อยืดเยิ้ม', 1, 6500.00, 2, 'upload/image/3', NULL, '2023-10-14 08:20:43');
 
 -- --------------------------------------------------------
 
@@ -172,7 +197,7 @@ CREATE TABLE `test` (
 --
 
 CREATE TABLE `users` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
@@ -181,6 +206,14 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Nawin Khamchun', 'nawin3399@gmail.com', NULL, '$2y$10$zWL5juwNI4wHbOskDfsave2EpfrOHJLi.5fqw5qbC1.kzXQW/FuFS', NULL, NULL, NULL),
+(2, 'admin', 'admin1@gmail.com', NULL, '$2y$10$maNUALk6wM.Pgyx7R6rEG.BggEY1vl4hC/BUJv/9kobVPmnsLP1Vy', NULL, '2023-10-14 08:20:13', '2023-10-14 08:20:13');
 
 --
 -- Indexes for dumped tables
@@ -206,6 +239,21 @@ ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_detail_order_id_foreign` (`order_id`),
+  ADD KEY `order_detail_product_id_foreign` (`product_id`);
+
+--
 -- Indexes for table `password_resets`
 --
 ALTER TABLE `password_resets`
@@ -225,12 +273,6 @@ ALTER TABLE `personal_access_tokens`
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`),
   ADD KEY `product_category_id_foreign` (`category_id`);
-
---
--- Indexes for table `test`
---
-ALTER TABLE `test`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -259,7 +301,19 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -271,23 +325,30 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT for table `test`
---
-ALTER TABLE `test`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD CONSTRAINT `order_detail_order_id_foreign` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`),
+  ADD CONSTRAINT `order_detail_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 
 --
 -- Constraints for table `product`
