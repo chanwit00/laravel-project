5cd0f7e133f31a0c8c1dda255a295acc7f42c7e9
PLUTo <nawin3399@gmail.com>
2023-10-15 00:58:47 +0700

    change username to email for login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,7 +15,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">ชื่อผู้ใช้</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-end">ชื่อ-นามสกุล</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text"
