c9fcd393c57bde0e5bee3374f5f5c102321ec6c6
PLUTo <nawin3399@gmail.com>
2023-10-15 15:04:42 +0700

    update example switch toggle and css

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.master') {{-- การสืบทอดโฟลเดอร์ --}}
 @section('title') BikeShop | อุปกรณ์จักรยาน, อะไหล่, ชุดแข่ง และอุปกรณ์ตกแต่ง @stop {{-- หัวข้อ title html --}}
 @section('content')
-<h1>ออเดอร์สินค้า</h1>
-<div class="container">
+    <h1>ข้อมูลสินค้า</h1>
+    <ul class="breadcrumb">
+        <li class="active">รายการข้อมูลสั่งซื้อ</li>
+    </ul>
+
     <div class="panel panel-default">
         <div class="panel-heading">
             <div class="panel-title">
-                <strong>รายการออเดอร์</strong>
+                <strong>รายการข้อมูลสั่งซื้อ</strong>
             </div>
         </div>
         <div class="panel-body">
-                {{ csrf_field() }}
-             
+            {{ csrf_field() }}
+
             </form>
             <br>
             <table class="table table-bordered bs-table">
@@ -28,22 +31,44 @@
                 <tbody>
                     @foreach ($orders as $o)
                         <tr>
-                            <td> {{$o->id}} </td>
-                            <td> {{$o->ref_id}} </td>
-                            <td> {{$o->user->name}} </td>
-                            <td> {{ $o->created_at->format("Y/m/d") }} </td>
-                            <td class="bs-center"> <a href="{{url('/order/detail/'.$o->id)}}" class="btn btn-info">รายละเอียด</a> </td>
-                            <td class="bs-center"> 
+                            <td> {{ $o->id }} </td>
+                            <td> {{ $o->ref_id }} </td>
+                            <td> {{ $o->user->name }} </td>
+                            <td> {{ $o->created_at->format('Y/m/d') }} </td>
+                            <td class="bs-center"> <a href="{{ url('/order/detail/' . $o->id) }}"
+                                    class="btn btn-info">รายละเอียด</a> </td>
+                            <td class="bs-center">
                                 @if ($o->payment_status == 0)
                                     <span class="label label-danger">ยังไม่ชำระเงิน</span>
                                 @else
                                     <span class="label label-success">ชำระเงินแล้ว</span>
                                 @endif
+
+                                <label class="switch">
+                                    @if ($o->payment_status == 0)
+                                        <input type="checkbox" id="toggleSwitch" checked>
+                                        <span class="slider" id="result"></span>
+                                    @else
+                                        <input type="checkbox" id="toggleSwitch">
+                                        <span class="slider" id="result"></span>
+                                    @endif
+                                </label>
                             </td>
                         </tr>
-                    
                     @endforeach
 
                 </tbody>
             </table>
+        </div>
+    </div>
+    <script>
+        const toggleSwitch = document.getElementById('toggleSwitch');
+        toggleSwitch.addEventListener('click', () => {
+            if (toggleSwitch.checked) {
+                console.log('Toggle ' + toggleSwitch.checked);
+            } else {
+                console.log('Toggle ' + toggleSwitch.checked);
+            }
+        });
+    </script>
 @endsection
